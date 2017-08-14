5f8a8807b061528d19528a8a9469d9f7924cd5d4
Will Feng <willfeng@fb.com>
2017-08-15 00:55:22 +0200

    testing

--- parlai/mturk/core/server/server.js
+++ parlai/mturk/core/server/server.js
@@ -54,9 +54,9 @@ app.get('/chat_index', async function (req, res) {
   if ((!changing_conversation) && (await data_model.hit_record_count(task_group_id, hit_id)) == _load_hit_config()['num_assignments']) {
     res.send('Sorry, there is no HIT available in this group right now.')
   }
-  else if ((await data_model.assignment_record_exists(task_group_id, assignment_id)) && !(await data_model.assignment_worker_record_exists(task_group_id, assignment_id, worker_id))) { // This HIT is already returned by another worker
-    res.send('Sorry, this HIT has been worked on by someone else before and will be expired soon. If there is "Skip HIT" button, please click on it to find out if the next HIT is available.');
-  }
+  // else if ((await data_model.assignment_record_exists(task_group_id, assignment_id)) && !(await data_model.assignment_worker_record_exists(task_group_id, assignment_id, worker_id))) { // This HIT is already returned by another worker
+  //   res.send('Sorry, this HIT has been worked on by someone else before and will be expired soon. If there is "Skip HIT" button, please click on it to find out if the next HIT is available.');
+  // }
   else if (assignment_id === 'ASSIGNMENT_ID_NOT_AVAILABLE') {
     template_context['is_cover_page'] = true;
     res.render('cover_page.html', template_context);
